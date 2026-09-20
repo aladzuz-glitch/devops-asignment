51484fed9d54274daa959097ef4051e3c2463ef5
Ajdin Aladžuz <ajdin.aladzuz@uniqa.ba>
2026-09-20 23:49:37 +0200

    Run pytest in CI

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from app import app
+
+
+def test_health():
+
+    response = app.test_client().get("/health")
+
+    assert response.status_code == 200
+
+    assert response.get_json() == {
+        "status": "healthy"
+    }
