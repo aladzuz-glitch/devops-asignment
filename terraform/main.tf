03054f6534576de01ecb58e6b0ffc87d5724ffe5
Ajdin Aladžuz <ajdin.aladzuz@uniqa.ba>
2026-09-21 23:05:58 +0200

    Add storage and container on Azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,13 +36,34 @@ resource "azurerm_linux_web_app" "devops_app" {
     Owner       = var.owner
   }
 
+
   site_config {
     always_on = false
   }
+
 
   app_settings = {
     APP_ENV     = var.environment
     APP_OWNER   = var.owner
     APP_VERSION = var.app_version
   }
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "ajtfstate2026"
+  resource_group_name      = azurerm_resource_group.devops_rg.name
+  location                 = azurerm_resource_group.devops_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Environment = var.environment
+    Owner       = var.owner
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
 }
